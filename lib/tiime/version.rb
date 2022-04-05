04b186f576757e636e62d084d41e7bdbd93e7774
Manuel Tancoigne <manu@opus-labs.fr>
2022-04-05 12:19:49 +0200

    Initial gem generation

---
+++ lib/tiime/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Tiime
+  VERSION = "0.1.0"
+end
---
+++ spec/tiime_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Tiime do
+  it "has a version number" do
+    expect(Tiime::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
