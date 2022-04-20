620ec9b172d380c28d719cda2e42dc7bef2c3c5a
Manuel Tancoigne <manu@opus-labs.fr>
2022-04-20 11:54:19 +0200

    Fix missing requires in Tiime module

--- lib/tiime.rb
+++ lib/tiime.rb
@@ -4,6 +4,9 @@ require 'tiime/bank_account'
 require 'tiime/bank_transaction'
 require 'tiime/company'
 require 'tiime/customer'
+require 'tiime/document'
+require 'tiime/document_category'
+require 'tiime/invoice'
 
 require 'tiime/version'
 
