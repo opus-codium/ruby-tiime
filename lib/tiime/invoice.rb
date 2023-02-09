4453818f6b8f20238801e64733d2f143e7fd42ce
Romuald Conty <romuald@opus-codium.fr>
2023-02-09 11:08:34 +0100

    Invoice: Allow to download PDF content

--- lib/tiime/invoice.rb
+++ lib/tiime/invoice.rb
@@ -6,6 +6,7 @@ module Tiime
   class Invoice < BaseModel
     get :all, '/companies/#company_id/invoices'
     get :find, '/companies/#company_id/invoices/:id'
+    get :pdf, '/companies/#company_id/invoices/pdf?id=:id', plain: true
 
     before_request :cache_refresh
     def cache_refresh(name, request)
