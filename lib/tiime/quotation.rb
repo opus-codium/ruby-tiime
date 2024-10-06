39167af54957d054e019dbd5e7ccbbdb8fd5cd41
François de Metz <francois@2metz.fr>
2024-10-06 17:55:19 +0200

    Add patch update to quotation.

--- lib/tiime/quotation.rb
+++ lib/tiime/quotation.rb
@@ -4,9 +4,12 @@ require 'tiime/base_model'
 
 module Tiime
   class Quotation < BaseModel
+    request_body_type :json
+
     get :all, '/companies/#company_id/quotations'
     get :find, '/companies/#company_id/quotations/:id'
     get :pdf, '/companies/#company_id/quotations/:id/pdf', plain: true
+    patch :update, '/companies/#company_id/quotations/:id', only_changed: true
 
     before_request :cache_refresh
     def cache_refresh(name, request)
